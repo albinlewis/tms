23e95597e27b022b8a3675213debce22a0cd3c46
Jamie John Armstrong-Arndt <JamieArmstrongArndt@users.noreply.github.com>
2017-11-30 17:15:58 +0100

    Mongoose Task Model

--- src/api/models/task.js
+++ src/api/models/task.js
@@ -1,1 +1,37 @@
-// mongoose model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Task Model
+const taskSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description: String,
+    created_at: {
+        type: Date,
+        default: new Date()
+    },
+    updated_at: {
+        type: Date,
+        default: new Date()
+    },
+    active:{
+        type: Boolean,
+        default: false
+    },
+    visible:{
+        type: Boolean,
+        default: true
+    },
+    done:{
+        type: Boolean,
+        default: false
+    },
+    interval:{
+        type: JSON,
+        default: {hasInterval: false, value: 0, unit: null}
+    }
+});
+
+module.exports = mongoose.model('Task', taskSchema);

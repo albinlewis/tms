c58a7f9937aaf24d71b3eab5c2d612dd81fd291c
Maximilian Belling <max-belling@web.de>
2017-11-30 18:09:03 +0100

    Started implementing Backend

--- src/api/task.js
+++ src/api/task.js
@@ -1,1 +1,12 @@
-// rest api handler for mongoose model
+const settings = require('../../settings');
+const Task = require('./models/task');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect(settings.user.mLab, {
+    useMongoClient: true
+});
+
+exports.notImplemented = (req, res) => {
+    res.status(200).send({ message: "route not implemented yet." });
+}

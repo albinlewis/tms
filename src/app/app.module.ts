e856164bc146b79fe6ed4f1e25fd5e9cd6af9f5d
Jamie John Armstrong-Arndt <JamieArmstrongArndt@users.noreply.github.com>
2017-12-04 19:11:31 +0100

    Material Task Component (Expandable)
    Adding the first template for component Expandable Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { TaskComponent } from './models/task/task.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]

a68a85e514963dc138e32493a7e691e458951ad1
Jamie John Armstrong-Arndt <JamieArmstrongArndt@users.noreply.github.com>
2017-12-05 17:32:09 +0100

    More Components for Frontend (Angular Material Components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { TaskComponent } from './models/task/task.component'
+import {MatExpansionModule, MatProgressBarModule, MatCardModule, MatGridListModule} from '@angular/material';
+import { TaskComponent } from './components/task/task.component';
+import { ProgressbarComponent } from './components/progressbar/progressbar.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    ProgressbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatProgressBarModule, 
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

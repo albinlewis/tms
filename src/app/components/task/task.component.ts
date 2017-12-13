a84433b8fe2dc003e0c37a7e177805d24cebb459
Timeless <maximilian2304@t-online.de>
2017-12-13 10:03:56 +0100

    tasks view

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'task',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
+  panelOpenState: boolean = false;
+
+  @Input()
+  task: Task;
+
+  @Output()
+  updateTask: EventEmitter<Task> = new EventEmitter();
+
+  @Output()
+  delete: EventEmitter<Task> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  deleteTask(task: Task) {
+    this.delete.emit(task);
+  }
+
+  updatefullTask(task: Task) {
+    this.updateTask.emit(task);
   }
 
 }
---
+++ src/app/components/tasks-view/tasks-view.component.ts
@@ -0,0 +1,71 @@
+import { Task } from './../../models/task';
+import { AppDataService } from './../../services/app-data.service';
+import { TaskComponent } from './../task/task.component';
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-tasks-view',
+  templateUrl: './tasks-view.component.html',
+  styleUrls: ['./tasks-view.component.scss']
+})
+export class TasksViewComponent implements OnInit {
+
+
+  tasklist: Task[] = [];
+  task: Task = new Task();
+  selectedTask: Task;
+
+  constructor(private dataService: AppDataService ) { }
+
+  ngOnInit() {
+    this.dataService.getAllTasks();
+    /*this.dataService.getAllTask()
+    .subscribe(t => {
+      console.log(t);
+      this.tasklist = t;
+    });*/
+  }
+
+  onselect(task: Task): void {
+    this.selectedTask = task;
+  }
+
+  onAddTask(task: Task) {
+    console.log(task);
+
+    this.tasklist.push(task);
+    this.dataService.addTask(task);
+    this.task = new Task();
+   // this.tasklist.push(task);
+    /*this.dataService.addTask(task)
+      .subscribe((t) => {
+        this.tasklist.push(t);
+      });*/
+  }
+  onDeleteTask(task) {
+    this.tasklist = this.tasklist.filter((t) => t.title !== task.title);
+
+    /*this.dataService.deleteTask(task)
+    .subscribe((res) => {
+      this.tasklist = this.tasklist
+        .filter((t) => t.id !== task.id);
+    });*/
+  }
+
+  onupdateTask(task) {
+
+
+    /*this.dataService.updateTaskById(task)
+    .subscribe(newtask => {
+      task = newtask;
+    });*/
+  }
+
+  getTasks() {
+    this.dataService.getAllTasks();
+    console.log();
+  }
+
+}

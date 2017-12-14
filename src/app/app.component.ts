c3bb3c93185cc8f1b6e7c6f44355c21d9f7bbfa4
Jamie John Armstrong-Arndt <JamieArmstrongArndt@users.noreply.github.com>
2017-12-14 12:35:07 +0100

    Including TaskDataService and APIService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { TaskDataService } from './services/task-data-service';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TaskDataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Task Management System';
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskDataService) {
+
+  }
+
+  public ngOnInit() {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  // ToDo: Event Handler (onAddTask, onDeleteTask, ...)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,39 @@
 import { RoutesModule } from './components/routes/routes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MatExpansionModule, MatProgressBarModule, MatCardModule, MatGridListModule, MatStepperModule} from '@angular/material';
-import { TaskComponent } from './components/task/task.component';
+import { MatExpansionModule, MatProgressBarModule, MatCardModule, MatGridListModule, MatStepperModule } from '@angular/material';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { TaskgridComponent } from './components/taskgrid/taskgrid.component';
 import { NotetrackerComponent } from './components/notetracker/notetracker.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ApiService } from './services/api-service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
     ProgressbarComponent,
     MenuComponent,
     TaskgridComponent,
     NotetrackerComponent,
-    TimerComponent,
+    TimerComponent
   ],
   imports: [
+    FormsModule,
+    HttpModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    MatProgressBarModule, 
+    MatProgressBarModule,
     MatCardModule,
     MatGridListModule,
     MatStepperModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

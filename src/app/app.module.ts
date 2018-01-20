b4b77a089d25a90119969ad17237ff6eed7cc946
Kouatcho Albin <albingeraud@yahoo.fr>
2018-01-21 00:02:02 +0100

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,40 @@
-import { Task } from './models/task';
-import { TasksViewComponent } from './components/tasks-view/tasks-view.component';
-import { TaskDetailComponent } from './components/task-detail/task-detail.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import {Task} from './models/task';
+import {TasksViewComponent} from './components/tasks-view/tasks-view.component';
+import {TaskDetailComponent} from './components/task-detail/task-detail.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
 import {
   MatSlideToggleModule, MatSelectModule, MatExpansionModule, MatProgressBarModule, MatCardModule,
   MatGridListModule, MatStepperModule, MatInputModule, MatButtonModule, MatTooltipModule, MatDialogModule,
   MatIconModule, MatTableDataSource, MatOptionModule, MatTable, MatTableModule
 } from '@angular/material';
-import { ProgressbarComponent } from './components/progressbar/progressbar.component';
-import { TaskgridComponent } from './components/taskgrid/taskgrid.component';
-import { NotetrackerComponent } from './components/notetracker/notetracker.component';
-import { TimerComponent } from './components/timer/timer.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { ApiService } from './services/api-service';
-import { TaskDataService } from './services/task-data-service';
-import { AddtaskComponent } from './components/addtask/addtask.component';
-import { TaskDetailPipe } from './components/task-detail/task-detail.pipe';
-import { DatabaseSelectorComponent, DatabaseDialog } from './components/database-selector/database-selector.component';
-import { NoTaskComponent } from './components/notasks/notasks.component';
-import { TaskExportComponent } from './components/export/export.component';
-import { HttpClientModule } from '@angular/common/http';
+import {ProgressbarComponent} from './components/progressbar/progressbar.component';
+import {TaskgridComponent} from './components/taskgrid/taskgrid.component';
+import {NotetrackerComponent} from './components/notetracker/notetracker.component';
+import {TimerComponent} from './components/timer/timer.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {ApiService} from './services/api-service';
+import {TaskDataService} from './services/task-data-service';
+import {AddtaskComponent} from './components/addtask/addtask.component';
+import {TaskDetailPipe} from './components/task-detail/task-detail.pipe';
+import {DatabaseSelectorComponent, DatabaseDialog} from './components/database-selector/database-selector.component';
+import {NoTaskComponent} from './components/notasks/notasks.component';
+import {TaskExportComponent} from './components/export/export.component';
+import {HttpClientModule} from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { ChartsModule} from 'ng2-charts';
-import { FilterbarComponent } from './components/filterbar/filterbar.component';
+import {ChartsModule} from 'ng2-charts';
+import {FilterbarComponent} from './components/filterbar/filterbar.component';
 
 
+import {PiechartComponent} from './components/statistics/piechart/piechart.component';
+import {BarchartComponent} from './components/statistics/barchart/barchart.component';
+import {TableComponent} from './components/statistics/table/table.component';
+import {StatisticsComponent} from './components/statistics/statistics.component';
 
-
-
-import { PiechartComponent } from './components/statistics/piechart/piechart.component';
-import { BarchartComponent } from './components/statistics/barchart/barchart.component';
-import { TableComponent } from './components/statistics/table/table.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-
-@NgModule ({
+@NgModule({
   declarations: [
     AddtaskComponent,
     AppComponent,
@@ -52,9 +49,12 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
     DatabaseSelectorComponent,
     DatabaseDialog,
     NoTaskComponent,
-    TaskExportComponent, 
+    TaskExportComponent,
     PiechartComponent,
-    FilterbarComponent
+    FilterbarComponent,
+    StatisticsComponent,
+    BarchartComponent,
+    TableComponent
   ],
   imports: [
     FormsModule,
@@ -86,4 +86,5 @@ import { StatisticsComponent } from './components/statistics/statistics.componen
   providers: [ApiService, TaskDataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

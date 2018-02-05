608e8e2f38545482e6b14311d0a7941d039732f3
Maximilian Belling <max-belling@web.de>
2018-02-05 15:42:01 +0100

    removed download api

--- src/api/routes.js
+++ src/api/routes.js
@@ -4,18 +4,14 @@ const jsonParser = bodyParser.json()
 module.exports = function (server) {
 
     const task = require('./task');
-    const prefix = '/api/tasks';
 
-    server.post(prefix + '/create', jsonParser, task.create);
-    server.get(prefix + '/find/all', task.findAll);
-    server.get(prefix + '/find/:id', task.findId);
-    server.put(prefix + '/update/:id', jsonParser, task.update);
-    server.delete(prefix + '/delete/:id', task.delete);
+    server.post('/api/tasks/create', jsonParser, task.create);
+    server.get('/api/tasks/find/all', task.findAll);
+    server.get('/api/tasks/find/:id', task.findId);
+    server.put('/api/tasks/update/:id', jsonParser, task.update);
+    server.delete('/api/tasks/delete/:id', task.delete);
 
     const mail = require('./mail');
     server.post('/api/mails/send', jsonParser, mail.sendMail);
-
-    const download = require('./download');
-    server.post('/api/download', jsonParser, download.getData);
 
 }

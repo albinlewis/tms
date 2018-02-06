f728cdb9f3115567adb10302e92ee70d381ca80b
mxxbe <max-belling@web.de>
2018-02-06 17:13:22 +0100

    modified piechart

--- src/app/components/statistics/piechart/piechart.component.ts
+++ src/app/components/statistics/piechart/piechart.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {TaskDataService} from '../../../services/task-data-service';
-import {Task} from '../../../models/task';
-import {UpdateService} from '../../../services/UpdateService';
+import { Component, OnInit } from '@angular/core';
+import { TaskDataService } from '../../../services/task-data-service';
+import { Task } from '../../../models/task';
+import { UpdateService } from '../../../services/UpdateService';
 
 
 @Component({
@@ -11,7 +11,7 @@ import {UpdateService} from '../../../services/UpdateService';
 })
 export class PiechartComponent {
   public tasks: Task[];
-
+  public totalTime: number = 0;
 
   // Pie
   public pieChartLabels: String[];
@@ -19,50 +19,68 @@ export class PiechartComponent {
   public pieChartType: String = 'pie';
 
   constructor(private taskService: TaskDataService, private u: UpdateService) {
-
     this.taskService.getTasks()
       .subscribe((tasks) => {
         this.tasks = tasks;
-        console.log(this.tasks);
-
+        this.tasks.forEach(el => {
+          this.totalTime += Number(el.time);
+        });
+        //console.log(this.tasks);
         this.display();
-
       });
     this.u.tasksUpdated
       .subscribe((task) => {
-      this.pieChartData = [];
+        this.pieChartData = [];
         this.tasks = task;
         this.display();
 
       });
-
-
   }
 
   // events
   public chartClicked(e: any): void {
-    console.log(e);
+    //console.log(e);
   }
 
   public chartHovered(e: any): void {
-    console.log(e);
+    //console.log(e);
   }
 
   display() {
     const piechartslabels: String[] = [];
-
     for (const eachObj of this.tasks) {
-
       piechartslabels.push(eachObj.title);
-      this.pieChartData.push(eachObj.time);
-
-
+      this.pieChartData.push(parseFloat((Number(eachObj.time) / 60).toFixed(2)));
     }
-
     this.pieChartLabels = piechartslabels;
-
-
   }
 
+  private formatTime(time) {
+    const seconds = this.pad(time % 60);
+    const minutes = this.pad((Math.floor(time / 60)) % 60);
+    const hours = this.pad(Math.floor((time / 60) / 60));
+    let paddedtime = '';
+
+    if (hours != '00') {
+      paddedtime = hours + 'h ' + minutes + 'm ' + seconds + 's';
+    }
+
+    if (hours == '00' && minutes != '00') {
+      paddedtime = minutes + 'm ' + seconds + 's';
+    }
+
+    if (minutes == '00' && seconds != '00') {
+      paddedtime = seconds + 's';
+    }
+
+    if (hours == '00' && minutes == '00' && seconds == '00') {
+      paddedtime = 'No time';
+    }
+
+    return paddedtime;
+  }
+  private pad(digit: any) {
+    return digit <= 9 ? '0' + digit : digit;
+  }
 
 }

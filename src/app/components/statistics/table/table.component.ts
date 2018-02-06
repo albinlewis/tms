da78ca136af93fbd68a4692e999d89d390777461
mxxbe <max-belling@web.de>
2018-02-06 17:12:46 +0100

    modified table view

--- src/app/components/statistics/table/table.component.ts
+++ src/app/components/statistics/table/table.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
-import {TaskDataService} from '../../../services/task-data-service';
-import {Task} from '../../../models/task';
-import {UpdateService} from '../../../services/UpdateService';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { TaskDataService } from '../../../services/task-data-service';
+import { Task } from '../../../models/task';
+import { UpdateService } from '../../../services/UpdateService';
 
 @Component({
   selector: 'app-table',
@@ -40,18 +40,18 @@ export class TableComponent {
 
   display() {
     const tablelabels: any[] = [];
-    console.log(this.element);
+    //console.log(this.element);
     let status: String;
 
     for (const eachObj of this.tasks) {
       if (eachObj.done === true) {
-        status = 'yes';
+        status = 'Done';
       } else {
-        status = 'no';
+        status = 'ToDo';
       }
 
       tablelabels.push(
-        {title: eachObj.title, time: eachObj.time, done: status}
+        { title: eachObj.title, time: this.formatTime(eachObj.time), done: status }
       );
 
 
@@ -59,10 +59,38 @@ export class TableComponent {
     this.element = tablelabels;
     this.displayedColumns = ['title', 'time', 'done'];
     this.dataSource = new MatTableDataSource<Element>(this.element);
-    console.log(this.element);
+    //console.log(this.element);
     for (let i = 0; i < 100; i++) {
 
     }
+  }
+
+  private formatTime(time) {
+    const seconds = this.pad(time % 60);
+    const minutes = this.pad((Math.floor(time / 60)) % 60);
+    const hours = this.pad(Math.floor((time / 60) / 60));
+    let paddedtime = '';
+
+    if (hours != '00') {
+      paddedtime = hours + 'h ' + minutes + 'm ' + seconds + 's';
+    }
+
+    if (hours == '00' && minutes != '00') {
+      paddedtime = minutes + 'm ' + seconds + 's';
+    }
+
+    if (minutes == '00' && seconds != '00') {
+      paddedtime = seconds + 's';
+    }
+
+    if (hours == '00' && minutes == '00' && seconds == '00') {
+      paddedtime = 'No time';
+    }
+
+    return paddedtime;
+  }
+  private pad(digit: any) {
+    return digit <= 9 ? '0' + digit : digit;
   }
 
 
@@ -74,7 +102,7 @@ export interface Element {
   weight: number;
   symbol: string;*/
   title: String;
-  time: Number;
+  time: String;
   done: Boolean;
 }
 
